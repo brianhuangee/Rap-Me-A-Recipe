a391ef57068696fd8ef4e96606f80dd3a340e6f9
Kirin Patel <kirinpatel@gmail.com>
2017-10-07 15:57:21 -0400

    Setup way to import API keys

--- src/main/java/utils/APIKeys.java
+++ src/main/java/utils/APIKeys.java
@@ -1,15 +1,34 @@
 package utils;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
 
 public class APIKeys {
 
-    ArrayList<String> keys = loadKeys();
+    public static HashMap<String, String> keys = loadKeys();
 
-    private ArrayList<String> loadKeys() {
-        ArrayList<String> keys = new ArrayList<>();
+    private static HashMap<String, String> loadKeys() {
+        HashMap<String, String> keys = new HashMap<>();
 
-        // TODO: (Kirin) load keys from file
+        Path path = Paths.get("keys");
+        try {
+            List<String> lines = Files.readAllLines(path);
+
+            for (String line : lines) {
+                String id = line.substring(0, line.indexOf('/') + 1);
+                String key = line.substring(line.indexOf('/') + 1);
+                keys.put(id, key);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return keys;
     }
