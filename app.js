1a0991d456e691dc914171822ef300ce3f87820e
Kirin Patel <kirinpatel@gmail.com>
2017-10-08 12:44:25 -0400

    Moved file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var SpotifyWebApi = require('spotify-web-api-node')
 var client_id = '4e9c3411c6944468b3aafb346a6f3ea0';
 var client_secret = '92c4313b40d64bdabe7597eb2d00ce50';
 var redirect_uri = 'http://localhost:8888/callback/';
-var player = require('./public/testing.js');
+var player = require('./testing.js');
 
 var app = express();
 
