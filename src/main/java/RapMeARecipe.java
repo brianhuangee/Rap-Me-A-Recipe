8c44adeadd5e9f6264895ed76bf6f6bc9c2d3941
Kirin Patel <kirinpatel@gmail.com>
2017-10-07 20:45:08 -0400

    Cleaned code and made methods simpler

--- src/main/java/RapMeARecipe.java
+++ src/main/java/RapMeARecipe.java
@@ -1,20 +1,6 @@
-import mccormick.JavaHTMLParse;
-
 public class RapMeARecipe {
 
     public static void main(String[] args) throws Exception {
 
-        JavaHTMLParse http = new JavaHTMLParse();
-
-
-
-        System.out.println("Testing 1 - Send Http GET request");
-        String html = http.sendGet();
-
-        html = html.substring(html.indexOf("itemid=\"")+9);
-        html = html.substring(0,html.indexOf("}"));
-        System.out.println(html);
     }
-
-
 }
