50a55365efe1bff090e445106a0270188f453267
Kirin Patel <kirinpatel@gmail.com>
2017-10-07 21:25:09 -0400

    Implemented way to get title and description of searched recipe

--- src/main/java/RapMeARecipe.java
+++ src/main/java/RapMeARecipe.java
@@ -1,8 +1,15 @@
-import spotify.Pull;
+import mccormick.Pull;
+import mccormick.Recipe;
 
 public class RapMeARecipe {
 
     public static void main(String[] args) {
-        Pull.pull();
+        try {
+            String recipe = Recipe.getRecipeDetails("pie");
+            System.out.println(Pull.getElementData("title", recipe));
+            System.out.println(Pull.getElementData("description", recipe));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
