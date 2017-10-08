26f61261f9a56594ef808e5d937b6e11f192ddd8
Christopher Mills <cltsd27@gmail.com>
2017-10-07 20:54:35 -0400

    Finished implementation of timestamped lyric search

--- src/main/java/musixmatch/MusixMatchApi.java
+++ src/main/java/musixmatch/MusixMatchApi.java
@@ -1,13 +1,20 @@
 package musixmatch;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 public class MusixMatchApi {
     private final String MUSIX_BASE = "http://api.musixmatch.com/ws/1.1/";
@@ -36,37 +43,101 @@ public class MusixMatchApi {
         } catch (IOException ignored) {}
         return buffer.toString();
     }
-    private String searchLyricWord(String word) {
-        final String request = SEARCH_METHOD + API_KEY_PARAM + MUSIX_API_KEY + "&q_lyrics=" + word +
-                "&f_music_genre_id=18&s_track_rating=desc&page=0&page_size=0";
+    private HashMap<String, String> searchLyricWord(String word, int page) throws UnsupportedEncodingException {
+        final String request = SEARCH_METHOD + API_KEY_PARAM + MUSIX_API_KEY + "&q_lyrics=" + URLEncoder.encode(word, "UTF-8") +
+                "&f_music_genre_id=18&s_track_rating=desc&page=" + page + "&page_size=1";
         final String response = requestApi(MUSIX_BASE, request);
         final JSONObject responseObj = new JSONObject(response);
         final JSONObject songObj = (JSONObject)
                 responseObj.getJSONObject("message").getJSONObject("body").getJSONArray("track_list").get(0);
         final JSONObject trackObj = songObj.getJSONObject("track");
-        return trackObj.getString("track_name") + trackObj.getString("artist_name");
+        HashMap<String, String> ret = new HashMap<>();
+        ret.put("title", trackObj.getString("track_name"));
+        ret.put("artist", trackObj.getString("artist_name"));
+        return ret;
 
     }
 
-    public String getTimestamp(String word) {
-        final String songNameAndArtist = searchLyricWord(word);
-        final String response = requestApi(YOUTUBE_BASE,
-                "&key=" + YOUTUBE_API_KEY + "&q=" + songNameAndArtist + "&maxResults=1");
-        final JSONObject responseObj = new JSONObject(response);
-        final String youtubeId =
-                ((JSONObject) responseObj.getJSONArray("items").get(0)).getJSONObject("id").getString("videoId");
+    public HashMap<String, String> getTimestamp(String word, int page, int timeout) throws IOException, SAXException, ParserConfigurationException {
+        HashMap<String, String> ret = new HashMap<>();
+        HashMap<String, String> songNameAndArtist = null;
         StringBuffer buffer = new StringBuffer();
-        try {
-            URL url = new URL("http://video.google.com/timedtext?lang=en&v=" + youtubeId);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-            String str;
-            while ((str = in.readLine()) != null) {
-                buffer.append(str);
+        while (buffer.toString().isEmpty()) {
+            timeout++;
+            if (timeout == 15) {
+                return null;
             }
-            in.close();
-        } catch (IOException ignored) {
+            try {
+                songNameAndArtist = searchLyricWord(word, page);
+            } catch (JSONException e) {
+                return null;
+            }
+
+            final String artist = songNameAndArtist.get("artist");
+            final String title = songNameAndArtist.get("title");
+            final String response = requestApi(YOUTUBE_BASE,
+                    "&type=video&videoCaption=closedCaption&key=" + YOUTUBE_API_KEY + "&q=" + URLEncoder.encode(title + " " + artist, "UTF-8") + "&maxResults=1");
+            final JSONObject responseObj = new JSONObject(response);
+            String youtubeId;
+            try {
+            if (!((JSONObject) responseObj.getJSONArray("items").get(0)).getJSONObject("snippet").getString("title").toLowerCase().contains(title.toLowerCase())) {
+                page++;
+                continue;
+            }
+                youtubeId =
+                        ((JSONObject) responseObj.getJSONArray("items").get(0)).getJSONObject("id").getString("videoId");
+            } catch (JSONException e) {
+                return null;
+            }
+            try {
+                URL url = new URL("http://video.google.com/timedtext?lang=en&v=" + youtubeId);
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    buffer.append(str);
+                }
+                in.close();
+            } catch (IOException ignored) {
+
+            }
+            page++;
         }
-        System.out.println(buffer.toString());
-        return "";
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(buffer.toString()));
+        Document document = builder.parse(is);
+        NodeList list = document.getElementsByTagName("text");
+        for (int i = 0; i < list.getLength(); ++i) {
+            Node item = list.item(i);
+            if (item.getTextContent().toLowerCase().contains(word.toLowerCase())) {
+                float start = Float.parseFloat(item.getAttributes().getNamedItem("start").getTextContent());
+                String[] line = item.getTextContent().toLowerCase().split(" ");
+                int syllableCount = 0;
+                int syllablesIn = 0;
+                EnglishSyllableCounter counter = new EnglishSyllableCounter();
+                for (String words : line) {
+                    if (words.toLowerCase().equals(word.toLowerCase()) && syllablesIn == 0){
+                        syllablesIn = syllableCount;
+                    }
+                   syllableCount += counter.countSyllables(words);
+                }
+                float dur = Float.parseFloat(item.getAttributes().getNamedItem("dur").getTextContent());
+                start += (syllablesIn/syllableCount) * dur;
+                float end = start + ((syllablesIn + counter.countSyllables(word))/(float) syllableCount) * dur;
+                if ((start == end && end == 0)) {
+                    continue;
+                }
+                if (end - start > 1) {
+                    end = start + 1;
+                }
+                ret.put("start", String.valueOf(start));
+                ret.put("end", String.valueOf(end));
+                ret.put("artist", songNameAndArtist.get("artist"));
+                ret.put("title", songNameAndArtist.get("title"));
+                return ret;
+            }
+        }
+        return getTimestamp(word, page + 1, timeout);
     }
 }
